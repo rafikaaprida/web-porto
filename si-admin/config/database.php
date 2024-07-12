717d329930aa161afbf16d861f315a4074eda820
rafikaaprida <Rafikanap8@gmail.com>
2024-07-12 23:35:21 +0700

    uas

--- si-admin/config/database.php
+++ si-admin/config/database.php
@@ -1,5 +1,6 @@
 <?php
-class Database {
+class Database
+{
     private $host = "andria-db.id.domainesia.com"; //domain
     private $database_name = "amisbud1_portofolio"; //nama database
     private $username = "amisbud1_portofolio"; //username phpmyadmin
@@ -7,13 +8,14 @@ class Database {
     private $charset = "utf8mb4";
     public $conn; //alias koneksi
 
-    public function getConnection(){
+    public function getConnection()
+    {
         $this->conn = null;
-        try{
+        try {
             $dsn = "mysql:host=$this->host;dbname=$this->database_name;charset=$this->charset";
-            $this->conn = new PDO( $dsn , $this->username, $this->password );
+            $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
+        } catch (PDOException $exception) {
             echo "Database could not be connected: " . $exception->getMessage();
         }
         return $this->conn;
